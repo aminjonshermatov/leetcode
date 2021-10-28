15a7fe17be4bb87769b48f45f65f903b1fdc77d2
aminjonshermatov <aminjonshermatov@gmail.com>
2021-10-28 23:06:04 +0300

    AddTwoNumbers

--- algorithms/AddTwoNumbers/AddTwoNumbers.cpp
+++ algorithms/AddTwoNumbers/AddTwoNumbers.cpp
@@ -2,36 +2,44 @@
 
 using namespace std;
 
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        // <value, index>
-        map<int, int> hashMap;
-
-        for (int i = 0; i < nums.size(); ++i) {
-            if (hashMap.count(target - nums[i]) > 0) {
-                return {hashMap[target - nums[i]], i};
-            }
-
-            hashMap.insert({nums[i], i});
-        }
-
-        return {};
-    }
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
-int main() {
-    Solution solution;
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        auto* resultList = new ListNode();
+        auto* lastEl = resultList;
+        int remainder = 0;
 
-    vector<int> input1 = {2,7,11,15};
-    for (const auto& num : solution.twoSum(input1, 9)) cout << num << ' ';
-    cout << '\n';
+        while (l1 != nullptr || l2 != nullptr) {
+            int sum = remainder;
+            remainder = 0;
 
-    vector<int> input2 = {3,2,4};
-    for (const auto& num : solution.twoSum(input2, 6)) cout << num << ' ';
-    cout << '\n';
+            if (l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
 
-    vector<int> input3 = {3,3};
-    for (const auto& num : solution.twoSum(input3, 6)) cout << num << ' ';
-    cout << '\n';
-}
+            if (l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            lastEl->next = new ListNode(sum % 10);
+            lastEl = lastEl->next;
+            if (sum > 9)
+                remainder = sum / 10;
+        }
+
+        if (remainder != 0) lastEl->next = new ListNode(remainder);
+
+        return resultList->next;
+    }
+};
