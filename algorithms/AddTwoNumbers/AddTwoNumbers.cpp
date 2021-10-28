ca02dfa8ad7466a8e99270a768ddd6708df76d6d
aminjonshermatov <aminjonshermatov@gmail.com>
2021-10-28 22:46:46 +0300

    Two Sum

--- algorithms/AddTwoNumbers/AddTwoNumbers.cpp
+++ algorithms/AddTwoNumbers/AddTwoNumbers.cpp
@@ -19,3 +19,19 @@ public:
         return {};
     }
 };
+
+int main() {
+    Solution solution;
+
+    vector<int> input1 = {2,7,11,15};
+    for (const auto& num : solution.twoSum(input1, 9)) cout << num << ' ';
+    cout << '\n';
+
+    vector<int> input2 = {3,2,4};
+    for (const auto& num : solution.twoSum(input2, 6)) cout << num << ' ';
+    cout << '\n';
+
+    vector<int> input3 = {3,3};
+    for (const auto& num : solution.twoSum(input3, 6)) cout << num << ' ';
+    cout << '\n';
+}
