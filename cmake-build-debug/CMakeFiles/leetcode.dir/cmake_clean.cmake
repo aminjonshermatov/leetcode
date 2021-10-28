4b0af97ab2646ea0b43d5bff1d0551f048dc7a74
aminjonshermatov <aminjonshermatov@gmail.com>
2021-10-28 21:58:36 +0300

    init

---
+++ cmake-build-debug/CMakeFiles/leetcode.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/leetcode.dir/1.TwoSum/solution.cpp.obj"
+  "CMakeFiles/leetcode.dir/main.cpp.obj"
+  "leetcode.exe"
+  "leetcode.exe.manifest"
+  "leetcode.pdb"
+  "libleetcode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/leetcode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
