f43a646233c02aa99f9cc7868d10e8e193b1ae6b
aminjonshermatov <aminjonshermatov@gmail.com>
2021-11-02 18:45:29 +0300

    LongestPalindromicSubstring

--- algorithms/LongestPalindromicSubstring/LongestPalindromicSubstring.cpp
+++ algorithms/LongestPalindromicSubstring/LongestPalindromicSubstring.cpp
@@ -1,4 +1,35 @@
-//
-// Created by aminjonshermatov on 10/29/2021.
-//
+#include <bits/stdc++.h>
 
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string res;
+        int len = s.size();
+
+        for (int k = 0; k < len; ++k) {
+            int l, r;
+            l = r = k;
+            while (l >= 0 && r < len && s[l] == s[r]) {
+                if (r - l + 1 > res.size()) {
+                    res = s.substr(l, r - l + 1);
+                }
+                --l;
+                ++r;
+            }
+
+            l = k;
+            r = k + 1;
+            while (l >= 0 && r < len && s[l] == s[r]) {
+                if (r - l + 1 > res.size()) {
+                    res = s.substr(l, r - l + 1);
+                }
+                --l;
+                ++r;
+            }
+        }
+
+        return res;
+    }
+};
