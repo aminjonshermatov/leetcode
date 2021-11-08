73f50b60fb0e272a0af14a24f7bb75ddfc37f8f5
aminjonshermatov <aminjonshermatov@gmail.com>
2021-11-08 18:47:03 +0300

    LowestCommonAncestorOfABinarySearchTree

--- algorithms/LowestCommonAncestorOfABinarySearchTree/LowestCommonAncestorOfABinarySearchTree.cpp
+++ algorithms/LowestCommonAncestorOfABinarySearchTree/LowestCommonAncestorOfABinarySearchTree.cpp
@@ -12,9 +12,21 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (p->val > q->val) return lowestCommonAncestor(root, q, p);
 
+        TreeNode *res = root, *cur = root;
+
+        while (cur != nullptr) {
+            res = cur;
+            if (cur->val >= p->val && cur->val <= q->val) {
+                break;
+            } else if (cur->val <= p->val && cur->val <= q->val) {
+                cur = cur->right;
+            } else {
+                cur = cur->left;
+            }
+        }
+
+        return res;
     }
 };
-
-int main() {
-}
