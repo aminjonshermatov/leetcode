4e373af47888dbe00160050ca077329861bbeb38
aminjonshermatov <aminjonshermatov@gmail.com>
2022-01-02 14:13:48 +0300

    RotatArray

--- algorithms/RotatArray/RotatArray.cpp
+++ algorithms/RotatArray/RotatArray.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 class Solution {
 public:
+    // O(n) - TC, O(n) - SC
     void rotate(vector<int>& nums, int k) {
         auto temp{nums};
         int len = nums.size();
@@ -15,5 +16,15 @@ public:
             int mod_ = (i + k) % len;
             nums[mod_] = temp[i];
         }
+    }
+
+    // O(n) - TC, O(1) - SC
+    void rotate(vector<int>& nums, int k) {
+        k = k % nums.size();
+        if (k == 0) return;
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
