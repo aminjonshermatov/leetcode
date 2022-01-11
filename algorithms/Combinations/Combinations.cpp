348321b2eb107a3e1309c255654d4565cbc7f45c
aminjonshermatov <aminjonshermatov@gmail.com>
2022-01-11 16:02:04 +0300

    Combinations

--- algorithms/Combinations/Combinations.cpp
+++ algorithms/Combinations/Combinations.cpp
@@ -8,23 +8,23 @@ public:
         vector<vector<int>> res;
         vector<int> cur;
 
-        backtrack(res, cur, 0, k, n);
+        function<void(int)> backtrack = [&](int idx) -> void {
+            int len = cur.size();
+
+            if (len == k) {
+                res.emplace_back(cur);
+                return;
+            }
+
+            for (int i = idx; i < n; ++i) {
+                cur.push_back(i + 1);
+                backtrack(i + 1);
+                cur.pop_back();
+            }
+        };
+
+        backtrack(0);
 
         return res;
-    }
-
-    void backtrack(vector<vector<int>>& res, vector<int>& cur, int idx, int k, int n) {
-        int len = cur.size();
-
-        if (len == k) {
-            res.push_back(cur);
-            return;
-        }
-
-        for (int i = idx; i < n; ++i) {
-            cur.push_back(i + 1);
-            backtrack(res, cur, i + 1, k, n);
-            cur.pop_back();
-        }
     }
 };
