67acfc18a9be791a9438a424c4dfc57544cd840a
aminjonshermatov <aminjonshermatov@gmail.com>
2022-01-12 15:15:20 +0300

    ClimbingStairs

--- algorithms/ClimbingStairs/ClimbingStairs.cpp
+++ algorithms/ClimbingStairs/ClimbingStairs.cpp
@@ -16,4 +16,17 @@ public:
 
         return dp[n];
     }
+
+    int climbStairs(int n) {
+        int prev = 1,
+                cur = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            auto temp{cur};
+            cur += prev;
+            prev = temp;
+        }
+
+        return cur;
+    }
 };
