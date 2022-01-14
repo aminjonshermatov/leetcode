82f956b7d020caf85e7c4f2fd3898a96c5eaa6da
aminjonshermatov <aminjonshermatov@gmail.com>
2022-01-14 14:56:46 +0300

    ContainsDuplicate

--- algorithms/ContainsDuplicate/ContainsDuplicate.cpp
+++ algorithms/ContainsDuplicate/ContainsDuplicate.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 class Solution {
 public:
+    // O(n*logn) - TC, O(1) - SC
     bool containsDuplicate(vector<int>& nums) {
         sort(begin(nums), end(nums));
 
@@ -12,6 +13,18 @@ public:
         for (int i = 1; i < len; ++i) {
             if (nums[i - 1] == nums[i])
                 return true;
+        }
+
+        return false;
+    }
+
+    // O(n) - TC, O(n) - SC
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> hash_set;
+
+        for (const auto &n : nums) {
+            if (hash_set.count(n) > 0) return true;
+            hash_set.insert(n);
         }
 
         return false;
