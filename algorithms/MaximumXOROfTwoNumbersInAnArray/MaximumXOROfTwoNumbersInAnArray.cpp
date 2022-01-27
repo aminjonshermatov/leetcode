a1028846be17b8b0f1654b7bd205b7a4fdf2e063
aminjonshermatov <aminjonshermatov@gmail.com>
2022-01-27 15:33:01 +0300

    MaximumXOROfTwoNumbersInAnArray

--- algorithms/MaximumXOROfTwoNumbersInAnArray/MaximumXOROfTwoNumbersInAnArray.cpp
+++ algorithms/MaximumXOROfTwoNumbersInAnArray/MaximumXOROfTwoNumbersInAnArray.cpp
@@ -5,6 +5,24 @@ using namespace std;
 class Solution {
 public:
     int findMaximumXOR(vector<int>& nums) {
+        int mask = 0, res = 0;
 
+        for (int i = 30; i >= 0; --i) {
+            mask = mask | (1 << i);
+
+            unordered_set<int> cur;
+
+            for (const auto &n : nums) cur.insert(n & mask);
+
+            int temp = res | (1 << i);
+            for (const auto &n : cur) {
+                if (cur.count(n ^ temp) > 0) {
+                    res = temp;
+                    break;
+                }
+            }
+        }
+
+        return res;
     }
 };
