5df38d19fe0d465d7300a43ff146e4571e38c72b
aminjonshermatov <aminjonshermatov@gmail.com>
2022-01-28 12:32:51 +0300

    UniquePaths2

--- algorithms/UniquePaths2/UniquePaths2.cpp
+++ algorithms/UniquePaths2/UniquePaths2.cpp
@@ -18,8 +18,7 @@ public:
 
         for (int i = 1; i < r; ++i) {
             for (int j = 1; j < c; ++j) {
-                if (grid[i][j] == 1) dp[i][j] = 0;
-                else dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                dp[i][j] = grid[i][j] == 1 ? 0 : dp[i - 1][j] + dp[i][j - 1];
             }
         }
 
