ebfc3413808cf53d02803a8654a8c94ad54a339a
aminjonshermatov <aminjonshermatov@gmail.com>
2022-01-29 13:38:30 +0300

    MaximalSquare

--- algorithms/MaximalSquare/MaximalSquare.cpp
+++ algorithms/MaximalSquare/MaximalSquare.cpp
@@ -5,23 +5,23 @@ using namespace std;
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        int m = static_cast<int>(matrix.size()),
-                n = static_cast<int>(matrix[0].size());
+        const int   r = static_cast<int>(matrix.size()),
+                    c = static_cast<int>(matrix[0].size());
 
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<vector<int>> dp(r, vector<int>(c));
 
         int res = INT_MIN;
-        for (int i = 0; i < n; ++i) {
-            dp[m - 1][i] = matrix[m - 1][i] - '0';
-            res = max(res, dp[m - 1][i]);
+        for (int i = 0; i < r; ++i) {
+            dp[i][c - 1] = matrix[i][c - 1] - '0';
+            res = max(res, dp[i][c - 1]);
         }
-        for (int i = 0; i < m; ++i) {
-            dp[i][n - 1] = matrix[i][n - 1] - '0';
-            res = max(res, dp[i][n - 1]);
+        for (int j = 0; j < c; ++j) {
+            dp[r - 1][j] = matrix[r - 1][j] - '0';
+            res = max(res, dp[r - 1][j]);
         }
 
-        for (int i = m - 2; i >= 0; --i) {
-            for (int j = n - 2; j >= 0; --j) {
+        for (int i = r - 2; i >= 0; --i) {
+            for (int j = c - 2; j >= 0; --j) {
                 dp[i][j] = matrix[i][j] == '0' ? 0 : min(dp[i + 1][j], min(dp[i + 1][j + 1], dp[i][j + 1])) + 1;
                 res = max(res, dp[i][j]);
             }
