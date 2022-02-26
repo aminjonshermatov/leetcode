788fa60ed2968070abd21ec976d36b87d4c2d0c5
aminjonshermatov <aminjonshermatov@gmail.com>
2022-02-26 19:31:27 +0300

    MaxAreaOfIsland

--- algorithms/MaxAreaOfIsland/MaxAreaOfIsland.cpp
+++ algorithms/MaxAreaOfIsland/MaxAreaOfIsland.cpp
@@ -5,16 +5,19 @@ using namespace std;
 class Solution {
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        function<int(int, int)> dfs = [&dfs, &grid](int x, int y) -> int {
-            if (y < 0 || y >= grid.size() || x < 0 || x >= grid[0].size() || grid[y][x] == 0) return 0;
+        const int r = static_cast<int>(grid.size());
+        const int c = static_cast<int>(grid[0].size());
+
+        const function<int(const int, const int)> dfs = [&](const int x, const int y) -> int {
+            if (y < 0 || y >= r || x < 0 || x >= c || grid[y][x] == 0) return 0;
 
             grid[y][x] = 0;
             return 1 + dfs(x + 1, y) + dfs(x - 1, y) + dfs(x, y + 1) + dfs(x, y - 1);
         };
 
         int maxArea = 0;
-        for (int i = 0; i < grid.size(); ++i) {
-            for (int j = 0; j < grid[i].size(); ++j) {
+        for (int i = 0; i < r; ++i) {
+            for (int j = 0; j < c; ++j) {
                 if (grid[i][j] == 0) continue;
                 maxArea = max(maxArea, dfs(j, i));
             }
