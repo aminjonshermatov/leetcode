3ccbc2d46cc447d8eaf02e2d212f0ad3ffc58137
aminjonshermatov <aminjonshermatov@gmail.com>
2022-03-03 15:12:55 +0300

    rithmeticlices

--- algorithms/ArithmeticSlices/ArithmeticSlices.cpp
+++ algorithms/ArithmeticSlices/ArithmeticSlices.cpp
@@ -5,19 +5,16 @@ using namespace std;
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
-        int len = nums.size();
+        const int n = static_cast<int>(nums.size());
 
-        vector<int> dp(len - 1);
+        vector<int> diff(n - 1);
 
-        for (int i = 1; i < len; ++i) {
-            dp[i - 1] = nums[i] - nums[i - 1];
-        }
+        for (int i = 1; i < n; ++i) diff[i - 1] = nums[i] - nums[i - 1];
 
-        int res = 0,
-            k = 1;
+        int res = 0, k = 1;
 
-        for (int i = 1; i < len - 1; ++i) {
-            if (dp[i] == dp[i - 1]) res += k++;
+        for (int i = 1; i < n - 1; ++i) {
+            if (diff[i] == diff[i - 1]) res += k++;
             else k = 1;
         }
 
