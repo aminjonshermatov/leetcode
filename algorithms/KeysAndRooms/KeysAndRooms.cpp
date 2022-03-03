8fb0cf10ba4738e3d468b3fe075eb7ba997cec6c
aminjonshermatov <aminjonshermatov@gmail.com>
2022-03-03 15:31:28 +0300

    eysndooms

--- algorithms/KeysAndRooms/KeysAndRooms.cpp
+++ algorithms/KeysAndRooms/KeysAndRooms.cpp
@@ -5,31 +5,22 @@ using namespace std;
 class Solution {
 public:
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
-        const int n = static_cast<int>(rooms.size());
-
-        vector<bool> seen(n, false);
-        seen[0] = true;
+        unordered_set<int> visited;
+        visited.insert(0);
         queue<int> q;
         q.push(0);
 
         while (not q.empty()) {
-            int sz = static_cast<int>(q.size());
+            auto cur{q.front()}; q.pop();
 
-            while (sz-- > 0) {
-                auto it{q.front()}; q.pop();
-
-                seen[it] = true;
-
-                for (const auto idx : rooms[it]) {
-                    if (not seen[idx]) q.push(idx);
+            for (const auto room : rooms[cur]) {
+                if (visited.count(room) == 0) {
+                    visited.insert(room);
+                    q.push(room);
                 }
             }
         }
 
-        for (const auto seek : seen) {
-            if (not seek) return false;
-        }
-
-        return true;
+        return visited.size() == rooms.size();
     }
 };
