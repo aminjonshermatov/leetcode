3860a9dbe059be3347f4d7a43344e1da365ddaa0
aminjonshermatov <aminjonshermatov@gmail.com>
2022-03-09 19:02:15 +0300

    MinimumNumberOfVerticesToReachAllNodes

--- algorithms/MinimumNumberOfVerticesToReachAllNodes/MinimumNumberOfVerticesToReachAllNodes.cpp
+++ algorithms/MinimumNumberOfVerticesToReachAllNodes/MinimumNumberOfVerticesToReachAllNodes.cpp
@@ -5,13 +5,14 @@ using namespace std;
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        unordered_map<int, int> preceded;
+        vector<int> procede(n, 0);
 
-        for (const auto edge : edges) ++preceded[edge[1]];
+        for (const auto edge : edges) ++procede[edge[1]];
 
         vector<int> res;
+
         for (int i = 0; i < n; ++i) {
-            if (preceded[i] == 0) res.push_back(i);
+            if (procede[i] == 0) res.push_back(i);
         }
 
         return res;
