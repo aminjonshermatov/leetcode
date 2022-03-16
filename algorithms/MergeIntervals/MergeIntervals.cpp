8fb14bb429a95b2c44c430e5998cf1dd116778b9
aminjonshermatov <aminjonshermatov@gmail.com>
2022-03-16 20:17:31 +0300

    MergeIntervals

--- algorithms/MergeIntervals/MergeIntervals.cpp
+++ algorithms/MergeIntervals/MergeIntervals.cpp
@@ -5,35 +5,27 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        stack<pair<int, int>> st;
+        const int n = static_cast<int>(intervals.size());
 
-        sort(intervals.begin(), intervals.end(), [&](auto &l, auto &r) -> bool { return l[0] < r[0]; });
-        for (const auto& interval : intervals) {
-            int start = interval[0], end = interval[1];
-
-            if (st.empty()) {
-                st.push({start, end});
-                continue;
-            }
-
-            while (!st.empty() && st.top().second >= start) {
-                auto [ss, ee] = st.top();
-                start  = ss;
-                end = max(end, ee);
-                st.pop();
-            }
-
-            st.push({start, end});
-        }
+        sort(intervals.begin(), intervals.end(), [](const auto &lhs, const auto &rhs) -> bool {
+            if (lhs[0] == rhs[0]) return lhs[0] < rhs[0];
+            return lhs[0] < rhs[0];
+        });
 
         vector<vector<int>> res;
-        while (!st.empty()) {
-            auto [s, e] = st.top();
+
+        for (int i = 0; i < n; ++i) {
+            auto s{intervals[i][0]};
+            auto e{intervals[i][1]};
+
+            while (i + 1 < n and intervals[i + 1][0] <= e) {
+                e = max(e, intervals[i + 1][1]);
+                ++i;
+            }
+
             res.push_back({s, e});
-            st.pop();
         }
 
-        reverse(res.begin(), res.end());
         return res;
     }
 };
