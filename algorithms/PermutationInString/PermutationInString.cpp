33877a8e0a6be5a67975949c1b865c22402cff28
aminjonshermatov <aminjonshermatov@gmail.com>
2022-03-16 18:10:25 +0300

    PermutationInString

--- algorithms/PermutationInString/PermutationInString.cpp
+++ algorithms/PermutationInString/PermutationInString.cpp
@@ -5,41 +5,32 @@ using namespace std;
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if (s1.size() > s2.size()) return false;
+        const int n = s2.size();
+        const int k = s1.size();
 
-        vector<int> s1Count(26, 0), s2Count(26, 0);
+        if (k > n) return false;
 
-        for (int i = 0; i < s1.size(); ++i)  {
-            ++s1Count[s1[i] - 'a'];
-            ++s2Count[s2[i] - 'a'];
+        int s1F[26] = {0};
+        for (const auto ch : s1) ++s1F[ch - 'a'];
+
+        int s2F[26] = {0};
+        for (int i = 0; i < k; ++i) ++s2F[s2[i] - 'a'];
+
+        const function<bool()> check = [&]() -> bool {
+            for (int i = 0; i < 26; ++i) {
+                if (s1F[i] > s2F[i]) return false;
+            }
+
+            return true;
+        };
+
+        for (int l = 1, r = k; r < n; ++l, ++r) {
+            if (check()) return true;
+
+            --s2F[s2[l - 1] - 'a'];
+            ++s2F[s2[r] - 'a'];
         }
 
-        int matchCount = 0;
-
-        for (int i = 0; i < 26; ++i)
-            if (s1Count[i] == s2Count[i]) ++matchCount;
-
-        int l = 0, r = s1.size();
-
-        while (r < s2.size()) {
-            if (matchCount == 26) return true;
-
-            ++s2Count[s2[r] - 'a'];
-            if (s2Count[s2[r] - 'a'] == s1Count[s2[r] - 'a'])
-                ++matchCount;
-            else if (s2Count[s2[r] - 'a'] == s1Count[s2[r] - 'a'] + 1)
-                --matchCount;
-
-            --s2Count[s2[l] - 'a'];
-            if (s2Count[s2[l] - 'a'] == s1Count[s2[l] - 'a'])
-                ++matchCount;
-            else if (s2Count[s2[l] - 'a'] == s1Count[s2[l] - 'a'] - 1)
-                --matchCount;
-
-            ++l;
-            ++r;
-        }
-
-        return matchCount == 26;
+        return check();
     }
 };
