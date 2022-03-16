05216812cb556b6a37de87da73e57cd5cd70f359
aminjonshermatov <aminjonshermatov@gmail.com>
2022-03-16 14:56:30 +0300

    MoveZeroes

--- algorithms/MoveZeroes/MoveZeroes.cpp
+++ algorithms/MoveZeroes/MoveZeroes.cpp
@@ -5,12 +5,12 @@ using namespace std;
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int j = 0;
+        int idx{0};
 
         for (int i = 0; i < nums.size(); ++i) {
-            if (nums[i] != 0) nums[j++] = nums[i];
+            if (nums[i] != 0) nums[idx++] = nums[i];
         }
 
-        for (; j < nums.size(); ++j) nums[j] = 0;
+        while (idx < nums.size()) nums[idx++] = 0;
     }
 };
