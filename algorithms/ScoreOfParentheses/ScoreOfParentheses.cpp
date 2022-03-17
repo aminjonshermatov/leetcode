f4dc83967bdc208432907d356ebe4ae68c755e5e
aminjonshermatov <aminjonshermatov@gmail.com>
2022-03-17 17:42:41 +0300

    ScoreOfParentheses

--- algorithms/ScoreOfParentheses/ScoreOfParentheses.cpp
+++ algorithms/ScoreOfParentheses/ScoreOfParentheses.cpp
@@ -2,3 +2,22 @@
 
 using namespace std;
 
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        stack<int> st;
+        st.push(0);
+
+        for (const auto ch : s) {
+            if (ch == '(') st.push(0);
+            else {
+                auto cur{st.top()}; st.pop();
+                auto prev{st.top()}; st.pop();
+
+                st.push(prev + max(1, 2 * cur));
+            }
+        }
+
+        return st.top();
+    }
+};
