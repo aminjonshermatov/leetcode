54467674a0869c02f383159df3af47138650cbce
aminjonshermatov <aminjonshermatov@gmail.com>
2022-03-17 20:46:10 +0300

    LongestPalindromicSubstring

--- algorithms/LongestPalindromicSubstring/LongestPalindromicSubstring.cpp
+++ algorithms/LongestPalindromicSubstring/LongestPalindromicSubstring.cpp
@@ -4,27 +4,25 @@ using namespace std;
 
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        int len = s.size(),
-            lRes = 0,
-            rRes = 0;
+    string longestPalindrome(string str) {
+        const int n = static_cast<int>(str.size());
+        auto s{0};
+        auto e{0};
 
-        function<void(int, int)> compute = [&s, &lRes, &rRes](int l, int r) -> void {
-            while (l >= 0 && r < s.size() && s[l] == s[r]) {
-                if (r - l > rRes - lRes) {
-                    lRes = l;
-                    rRes = r;
-                }
-                --l;
-                ++r;
+        const function<void(int, int)> longestP = [&](int i, int j) -> void {
+            while (i >= 0 and j < n and str[i] == str[j]) {
+                if (e - s < j - i) s = i, e = j;
+
+                --i;
+                ++j;
             }
         };
 
-        for (int k = 0; k < len; ++k) {
-            compute(k, k);
-            compute(k, k + 1);
+        for (int i = 0; i < n; ++i) {
+            longestP(i, i);
+            longestP(i, i + 1);
         }
 
-        return s.substr(lRes, rRes - lRes + 1);
+        return str.substr(s, e - s + 1);
     }
 };
