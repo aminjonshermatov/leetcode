14e898d0819a2a0b8d45e1dceb98fd5467f55be5
aminjonshermatov <aminjonshermatov@gmail.com>
2022-03-22 12:09:48 +0300

    RangeSumQueryImmutable

--- algorithms/RangeSumQueryImmutable/RangeSumQueryImmutable.cpp
+++ algorithms/RangeSumQueryImmutable/RangeSumQueryImmutable.cpp
@@ -3,22 +3,22 @@
 using namespace std;
 
 class NumArray {
+    vector<int> pref;
+
 public:
-    vector<int> _prefixSum, _nums;
+    NumArray(vector<int>& nums) {
+        pref.resize(nums.size() + 1);
 
-    NumArray(vector<int>&& nums) {
-        this->_nums = nums;
-
-        size_t len = nums.size();
-        this->_prefixSum.resize(len);
-
-        this->_prefixSum[0] = nums[0];
-        for (size_t i = 1; i < len; ++i) {
-            this->_prefixSum[i] = this->_prefixSum[i - 1] + nums[i];
-        }
+        for (int i = 1; i <= nums.size(); ++i) pref[i] = pref[i - 1] + nums[i - 1];
     }
 
     int sumRange(int left, int right) {
-        return this->_prefixSum[right] - this->_prefixSum[left] + this->_nums[left];
+        return pref[right + 1] - pref[left];
     }
 };
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
