769bb041a4fcef24c6c1478b45feeaf8dd22d497
aminjonshermatov <aminjonshermatov@gmail.com>
2022-04-13 12:00:25 +0300

    SpiralMatrix2

--- algorithms/SpiralMatrix2/SpiralMatrix2.cpp
+++ algorithms/SpiralMatrix2/SpiralMatrix2.cpp
@@ -5,26 +5,16 @@ using namespace std;
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> matrix(n, vector<int>(n));
-        int leftTop = 0, rightBottom = n - 1, curN = 0;
+        vector<vector<int>> mat(n, vector<int>(n));
 
-        while (leftTop <= rightBottom) {
-            // left to right
-            for (int j = leftTop; j <= rightBottom; ++j) matrix[leftTop][j] = ++curN;
-
-            // top to bottom
-            for (int i = leftTop + 1; i <= rightBottom; ++i) matrix[i][rightBottom] = ++curN;
-
-            // right to left
-            for (int j = rightBottom - 1; j >= leftTop; --j) matrix[rightBottom][j] = ++curN;
-
-            // bottom to top
-            for (int i = rightBottom - 1; i > leftTop; --i) matrix[i][leftTop] = ++curN;
-
-            ++leftTop;
-            --rightBottom;
+        for (int lt = 0, rb = n - 1, k = 1; lt <= rb; ++lt, --rb) {
+            for (int j = lt; j <= rb; ++j) mat[lt][j] = k++;
+            for (int i = lt + 1; i < rb; ++i) mat[i][rb] = k++;
+            if (lt == rb) break;
+            for (int j = rb; j >= lt; --j) mat[rb][j] = k++;
+            for (int i = rb - 1; i > lt; --i) mat[i][lt] = k++;
         }
 
-        return matrix;
+        return mat;
     }
 };
