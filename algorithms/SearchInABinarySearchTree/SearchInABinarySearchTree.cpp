0934e6297fce5ce0a314d7ad005cc7c3458c40aa
aminjonshermatov <aminjonshermatov@gmail.com>
2022-04-14 15:21:53 +0300

    SearchInABinarySearchTree

--- algorithms/SearchInABinarySearchTree/SearchInABinarySearchTree.cpp
+++ algorithms/SearchInABinarySearchTree/SearchInABinarySearchTree.cpp
@@ -14,10 +14,9 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
-        if (root == nullptr) return root;
-
-        if (root->val == val) return root;
-        if (root->val > val) return searchBST(root->left, val);
-        return searchBST(root->right, val);
+        if (root == nullptr or root->val == val) return root;
+        return root->val > val
+               ? searchBST(root->left, val)
+               : searchBST(root->right, val);
     }
 };
