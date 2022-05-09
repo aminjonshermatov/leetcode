5f5300f28a60a8e977162f78bedcb0ac155d0873
aminjonshermatov <aminjonshermatov@gmail.com>
2022-05-09 12:24:23 +0300

    LetterCombinationsOfAPhoneNumber

--- algorithms/LetterCombinationsOfAPhoneNumber/LetterCombinationsOfAPhoneNumber.cpp
+++ algorithms/LetterCombinationsOfAPhoneNumber/LetterCombinationsOfAPhoneNumber.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 class Solution {
 public:
-    unordered_map<char, vector<string>> dic{
+    unordered_map<char, vector<string>> keys = {
             {'2', {"a", "b", "c"}},
             {'3', {"d", "e", "f"}},
             {'4', {"g", "h", "i"}},
@@ -12,23 +12,21 @@ public:
             {'6', {"m", "n", "o"}},
             {'7', {"p", "q", "r", "s"}},
             {'8', {"t", "u", "v"}},
-            {'9', {"w", "x", "y", "z"}}
+            {'9', {"w", "x", "y", "z"}},
     };
 
     // O(4^n) - TC, O(1) - SC (exclusive result array and hashMap)
     vector<string> letterCombinations(string digits) {
         if (digits.empty()) return {};
-        if (digits.size() == 1) return dic[digits[0]];
+        if (digits.size() == 1) return keys.at(digits.front());
 
-        vector<string> res,
-                       temp = letterCombinations(digits.substr(1, digits.size() - 1));
-
-        for (const auto& s : dic[digits[0]]) {
-            for (const auto& com : temp) {
-                res.push_back(s + com);
+        vector<string> res;
+        for (const auto &str : letterCombinations({next(digits.begin()), digits.end()})) {
+            for (const auto &d : keys.at(digits.front())) {
+                res.push_back(d + str);
             }
         }
 
         return res;
-    }
+    };
 };
