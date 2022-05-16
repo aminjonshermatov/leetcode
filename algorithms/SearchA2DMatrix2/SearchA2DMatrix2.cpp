865443876990a82aeb435f18b4833eae01dcad60
aminjonshermatov <aminjonshermatov@gmail.com>
2022-05-16 22:24:26 +0300

    SearchA2DMatrix2

--- algorithms/SearchA2DMatrix2/SearchA2DMatrix2.cpp
+++ algorithms/SearchA2DMatrix2/SearchA2DMatrix2.cpp
@@ -5,16 +5,14 @@ using namespace std;
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        const int n = static_cast<int>(matrix.size());
-        const int m = static_cast<int>(matrix[0].size());
+        const int n = (int)matrix.size();
+        const int m = (int)matrix[0].size();
 
-        if (target < matrix[0][0] or target > matrix[n - 1][m - 1]) return false;
+        int i = 0, j = m - 1;
 
-        int row = 0, col = m - 1;
-
-        while (row < n and col >= 0) {
-            if (matrix[row][col] < target) ++row;
-            else if (matrix[row][col] > target) --col;
+        while (i >= 0 && i < n && j >= 0 && j < m) {
+            if (matrix[i][j] < target) ++i;
+            else if (matrix[i][j] > target) --j;
             else return true;
         }
 
