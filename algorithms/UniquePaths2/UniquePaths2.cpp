5a3325fb736263fb01cd13370b0b8f8cc535e5e5
aminjonshermatov <aminjonshermatov@gmail.com>
2022-05-20 14:09:40 +0300

    UniquePaths2

--- algorithms/UniquePaths2/UniquePaths2.cpp
+++ algorithms/UniquePaths2/UniquePaths2.cpp
@@ -3,25 +3,23 @@
 using namespace std;
 
 class Solution {
+    int dp[101][101];
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        const int r = grid.size();
-        const int c = grid[0].size();
+        const int n(grid.size());
+        const int m(grid[0].size());
 
-        if (grid[0][0] == 1 or grid[r - 1][c - 1] == 1) return 0;
+        memset(dp, 0, sizeof(dp));
+        for (int j = 0; j < m && grid[0][j] != 1; ++j) dp[0][j] = 1;
+        for (int i = 0; i < n && grid[i][0] != 1; ++i) dp[i][0] = 1;
 
-        vector<vector<int>> dp(r, vector<int>(c));
-        dp[0][0] = 1;
-
-        for (int i = 1; i < r; ++i) dp[i][0] = grid[i][0] == 1 ? 0 : dp[i - 1][0];
-        for (int j = 1; j < c; ++j) dp[0][j] = grid[0][j] == 1 ? 0 : dp[0][j - 1];
-
-        for (int i = 1; i < r; ++i) {
-            for (int j = 1; j < c; ++j) {
-                dp[i][j] = grid[i][j] == 1 ? 0 : dp[i - 1][j] + dp[i][j - 1];
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j < m; ++j) {
+                if (grid[i][j] == 1) continue;
+                dp[i][j] += dp[i - 1][j] + dp[i][j - 1];
             }
         }
 
-        return dp[r - 1][c - 1];
+        return dp[n - 1][m - 1];
     }
 };
