4569faeee37208ca2c717fb8d29d6c6371fdfa8b
aminjonshermatov <aminjonshermatov@gmail.com>
2022-05-22 23:53:08 +0300

    PalindromicSubstrings

--- algorithms/PalindromicSubstrings/PalindromicSubstrings.cpp
+++ algorithms/PalindromicSubstrings/PalindromicSubstrings.cpp
@@ -22,3 +22,24 @@ public:
         return res;
     }
 };
+
+class Solution {
+    bool dp[1001][1001];
+
+public:
+    int countSubstrings(string s) {
+        memset(dp, 0, sizeof(dp));
+
+        int ans = 0;
+        for (int k = 0; k < s.size(); ++k) {
+            for (int i = 0, j = k; i < s.size() && j < s.size(); ++i, ++j) {
+                if (s[i] == s[j] && (j - i < 2 || dp[i + 1][j - 1])) {
+                    ++ans;
+                    dp[i][j] = true;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
