d78fa23f312f25a8e651db9faf316467f5f91a68
aminjonshermatov <aminjonshermatov@gmail.com>
2022-06-24 22:16:23 +0300

    ReverseLinkedList

--- algorithms/ReverseLinkedList/ReverseLinkedList.cpp
+++ algorithms/ReverseLinkedList/ReverseLinkedList.cpp
@@ -10,19 +10,26 @@ struct ListNode {
      ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if (head == nullptr || head->next == nullptr) return head;
-
-        ListNode *prev = nullptr, *cur = head, *next = head->next;
+        ListNode *prev = nullptr, *cur = head;
 
         while (cur != nullptr) {
+            auto next = cur->next;
             cur->next = prev;
             prev = cur;
             cur = next;
-            if (next != nullptr)
-                next = next->next;
         }
 
         return prev;
