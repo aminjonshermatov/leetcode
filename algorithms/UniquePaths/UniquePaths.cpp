0c81d677f8d64b5d56a1008d6892f23418c0330f
aminjonshermatov <aminjonshermatov@gmail.com>
2022-07-03 00:18:36 +0500

    UniquePaths

--- algorithms/UniquePaths/UniquePaths.cpp
+++ algorithms/UniquePaths/UniquePaths.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 
 class Solution {
+    int dp[101][101];
+
 public:
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+    int uniquePaths(int n, int m) {
+        for (int i = 0; i < n; ++i) dp[i][0] = 1;
+        for (int j = 1; j < m; ++j) dp[0][j] = 1;
 
-        for (int i = 1; i <= m; ++i) dp[1][i] = 1;
-        for (int i = 1; i <= n; ++i) dp[i][1] = 1;
-
-        for (int i = 2; i <= n; ++i) {
-            for (int j = 2; j <= m; ++j) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j < m; ++j) {
+                dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
             }
         }
 
-        return dp[n][m];
+        return dp[n - 1][m - 1];
     }
 };
