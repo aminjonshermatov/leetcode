a10ec19b0d0da6ece76bfb396e80b613d5f4e6f2
aminjonshermatov <aminjonshermatov@gmail.com>
2022-07-14 14:32:47 +0500

    ConstructBinaryTreeFromPreorderAndInorderTraversal

--- algorithms/ConstructBinaryTreeFromPreorderAndInorderTraversal/ConstructBinaryTreeFromPreorderAndInorderTraversal.cpp
+++ algorithms/ConstructBinaryTreeFromPreorderAndInorderTraversal/ConstructBinaryTreeFromPreorderAndInorderTraversal.cpp
@@ -16,15 +16,19 @@ using namespace std;
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        const function<TreeNode*(const int, const int, const int, const int)> helper = [&](const int pl, const int pr, const int il, const int ir) -> TreeNode* {
-            if (pr <= pl or ir <= il) return nullptr;
+        const function<TreeNode*(int, int, int, int)> helper = [&](int pl, int pr, int il, int ir) -> TreeNode* {
+            if (pl >= pr || il >= ir) return nullptr;
 
-            const int mid = static_cast<int>(find(inorder.begin(), inorder.end(), preorder[pl]) - inorder.begin()) - il;
-            return new TreeNode{preorder[pl], helper(pl + 1, pl + 1 + mid, il, il + mid), helper(pl + 1 + mid, pr, il + mid + 1, ir)};
+            int mid = find(inorder.begin(), inorder.end(), preorder[pl]) - inorder.begin() - il;
+            return new TreeNode{
+                preorder[pl],
+                helper(pl + 1, pl + 1 + mid, il, il + mid),
+                helper(pl + 1 + mid, pr, il + mid + 1, ir)
+            };
         };
 
-        const int pn = static_cast<int>(preorder.size());
-        const int in = static_cast<int>(inorder.size());
+        const int pn(preorder.size());
+        const int in(inorder.size());
 
         return helper(0, pn, 0, in);
     }
