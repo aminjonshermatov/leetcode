18345a97427ab5da4e875baf6520affdbd328591
aminjonshermatov <aminjonshermatov@gmail.com>
2022-07-15 21:59:03 +0500

    MaxAreaOfIslands

--- algorithms/MaxAreaOfIsland/MaxAreaOfIsland.cpp
+++ algorithms/MaxAreaOfIsland/MaxAreaOfIsland.cpp
@@ -5,24 +5,29 @@ using namespace std;
 class Solution {
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        const int r = static_cast<int>(grid.size());
-        const int c = static_cast<int>(grid[0].size());
+        const int n(grid.size());
+        const int m(grid[0].size());
 
-        const function<int(const int, const int)> dfs = [&](const int x, const int y) -> int {
-            if (y < 0 || y >= r || x < 0 || x >= c || grid[y][x] == 0) return 0;
+        array<int, 5> dk = { -1, 0, 1, 0, -1 };
 
-            grid[y][x] = 0;
-            return 1 + dfs(x + 1, y) + dfs(x - 1, y) + dfs(x, y + 1) + dfs(x, y - 1);
+        function<int(int, int)> dfs = [&](int i, int j) -> int {
+            if (i < 0 || i >= n || j < 0 || j >= m || grid[i][j] == 0) return 0;
+
+            int acc = 1;
+            grid[i][j] = 0;
+            for (int k = 0; k < 4; ++k) acc += dfs(i + dk[k], j + dk[k + 1]);
+
+            return acc;
         };
 
-        int maxArea = 0;
-        for (int i = 0; i < r; ++i) {
-            for (int j = 0; j < c; ++j) {
-                if (grid[i][j] == 0) continue;
-                maxArea = max(maxArea, dfs(j, i));
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] != 0) ans = max(ans, dfs(i, j));
             }
         }
 
-        return maxArea;
+
+        return ans;
     }
 };
