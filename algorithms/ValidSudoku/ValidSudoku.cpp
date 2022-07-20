c0ab4ffa6bcadd9ec9d0b6b7a6988b4b4fcea99a
aminjonshermatov <aminjonshermatov@gmail.com>
2022-07-21 00:46:03 +0500

    ValidSudoku

--- algorithms/ValidSudoku/ValidSudoku.cpp
+++ algorithms/ValidSudoku/ValidSudoku.cpp
@@ -21,4 +21,27 @@ public:
 
         return true;
     }
+
+    bool isValidSudoku(vector<vector<char>>& board) {
+        uint16_t rows[9] = {0,}, cols[9] = {0,}, sqr[9] = {0,};
+
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; ++j) {
+                if (!isdigit(static_cast<unsigned char>(board[i][j]))) continue;
+
+                int cur = board[i][j] - '1';
+                if ((rows[i] >> cur) & 1) return false;
+                rows[i] |= 1 << cur;
+
+                if ((cols[j] >> cur) & 1) return false;
+                cols[j] |= 1 << cur;
+
+                int idx = i / 3 * 3 + j / 3;
+                if ((sqr[idx] >> cur) & 1) return false;
+                sqr[idx] |= 1 << cur;
+            }
+        }
+
+        return true;
+    }
 };
