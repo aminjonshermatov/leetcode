67ced01b873bdfea2f373dd34b20c234c85f9822
aminjonshermatov <aminjonshermatov@gmail.com>
2022-07-21 23:44:09 +0500

    PermutationInString

--- algorithms/PermutationInString/PermutationInString.cpp
+++ algorithms/PermutationInString/PermutationInString.cpp
@@ -5,32 +5,33 @@ using namespace std;
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        const int n = s2.size();
-        const int k = s1.size();
+        const int k(s1.size());
+        const int n(s2.size());
 
-        if (k > n) return false;
-
-        int s1F[26] = {0};
-        for (const auto ch : s1) ++s1F[ch - 'a'];
-
-        int s2F[26] = {0};
-        for (int i = 0; i < k; ++i) ++s2F[s2[i] - 'a'];
-
-        const function<bool()> check = [&]() -> bool {
-            for (int i = 0; i < 26; ++i) {
-                if (s1F[i] > s2F[i]) return false;
-            }
-
-            return true;
-        };
-
-        for (int l = 1, r = k; r < n; ++l, ++r) {
-            if (check()) return true;
-
-            --s2F[s2[l - 1] - 'a'];
-            ++s2F[s2[r] - 'a'];
+        uint32_t s1_mask = 0u, s1_freq[26] = {0u,};
+        for (auto ch : s1) {
+            ++s1_freq[ch - 'a'];
+            s1_mask |= 1 << (ch - 'a');
         }
 
-        return check();
+        uint32_t w_mask = 0u, w_freq[26] = {0u,};
+        for (size_t l = 0, r = 0; r < n; ++r) {
+            w_mask |= 1 << (s2[r] - 'a');
+            ++w_freq[s2[r] - 'a'];
+
+            if (r - l + 1 > k) {
+                --w_freq[s2[l++] - 'a'];
+                if (w_freq[s2[l - 1] - 'a'] == 0) w_mask &= ~(1u << (s2[l - 1] - 'a'));
+            }
+
+
+            if ((w_mask & s1_mask) == s1_mask) {
+                bool ok = true;
+                for (size_t i = 0; i < 26 && ok; ++i) ok = s1_freq[i] == w_freq[i];
+
+                if (ok) return true;
+            }
+        }
+        return false;
     }
 };
