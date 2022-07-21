29939e8f91963d351b03ec941cf437dfaf3f4087
aminjonshermatov <aminjonshermatov@gmail.com>
2022-07-21 15:23:27 +0500

    ContainerWithMostWater

--- algorithms/ContainerWithMostWater/ContainerWithMostWater.cpp
+++ algorithms/ContainerWithMostWater/ContainerWithMostWater.cpp
@@ -5,16 +5,15 @@ using namespace std;
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        auto res{0};
-        int l = 0, r = static_cast<int>(height.size()) - 1;
+        int ans = 0;
 
-        while (l < r) {
-            res = max(res, (r - l) * min(height[l], height[r]));
+        for (int l = 0, r = static_cast<int>(height.size()) - 1; l < r;) {
+            ans = max(ans, (r - l) * min(height[l], height[r]));
 
             if (height[l] < height[r]) ++l;
             else --r;
         }
 
-        return res;
+        return ans;
     }
 };
