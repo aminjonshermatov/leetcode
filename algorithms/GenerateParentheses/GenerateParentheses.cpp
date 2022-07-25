239706e50a29577479d58b9fe7aafa6607fcbd1d
aminjonshermatov <aminjonshermatov@gmail.com>
2022-07-25 09:50:23 +0500

    GenerateParentheses

--- algorithms/GenerateParentheses/GenerateParentheses.cpp
+++ algorithms/GenerateParentheses/GenerateParentheses.cpp
@@ -1,34 +1,38 @@
+
 #include "bits/stdc++.h"
 
 using namespace std;
 
 class Solution {
+    int n;
+    vector<string> res;
+    string cur;
+
+    void backtrack(int open, int closed) {
+        if (open + closed == 2 * n) {
+            res.push_back(cur);
+            return;
+        }
+
+        if (open + 1 <= n) {
+            cur.push_back('(');
+            backtrack(open + 1, closed);
+            cur.pop_back();
+        }
+
+        if (closed + 1 <= open && open + 1 + closed <= 2 * n) {
+            cur.push_back(')');
+            backtrack(open, closed + 1);
+            cur.pop_back();
+        }
+    }
+
 public:
-    vector<string> generateParenthesis(int n) {
-        vector<string> res;
-        list<char> cur;
+    vector<string> generateParenthesis(int n_) {
+        n = n_;
+        cur.reserve(2 * n);
 
-        const function<void(const int, const int)> generate = [&](const int open, const int close) -> void {
-            if (open + close == 2 * n) {
-                res.emplace_back(cur.begin(), cur.end());
-                return;
-            }
-
-            if (open < n) {
-                cur.push_back('(');
-                generate(open + 1, close);
-                cur.pop_back();
-            }
-
-            if (close < open) {
-                cur.push_back(')');
-                generate(open, close + 1);
-                cur.pop_back();
-            }
-        };
-
-        generate(0, 0);
-
+        backtrack(0, 0);
         return res;
     }
 };
