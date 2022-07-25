2a72ad55053cc0fd698776a9866a2ee451ebc925
aminjonshermatov <aminjonshermatov@gmail.com>
2022-07-25 09:40:32 +0500

    EvaluateReversePolishNotation

--- algorithms/EvaluateReversePolishNotation/EvaluateReversePolishNotation.cpp
+++ algorithms/EvaluateReversePolishNotation/EvaluateReversePolishNotation.cpp
@@ -7,30 +7,22 @@ public:
     int evalRPN(vector<string>& tokens) {
         stack<int> st;
 
-        for (const auto &str : tokens) {
-            if (str == "/") {
-                auto last{st.top()}; st.pop();
-                auto preLast{st.top()}; st.pop();
+        for (const auto &s : tokens) {
+            if (isdigit(s.back())) {
+                st.push(stoi(s));
+            } else {
+                assert(st.size() > 1);
+                auto y = st.top(); st.pop();
+                auto x = st.top(); st.pop();
 
-                st.push(preLast / last);
-            } else if (str == "*") {
-                auto last{st.top()}; st.pop();
-                auto preLast{st.top()}; st.pop();
-
-                st.push(preLast * last);
-            } else if (str == "-") {
-                auto last{st.top()}; st.pop();
-                auto preLast{st.top()}; st.pop();
-
-                st.push(preLast - last);
-            } else if (str == "+") {
-                auto last{st.top()}; st.pop();
-                auto preLast{st.top()}; st.pop();
-
-                st.push(preLast + last);
-            } else st.push(stoi(str));
+                if (s == "+") st.push(x + y);
+                else if (s == "-") st.push(x - y);
+                else if (s == "*") st.push(x * y);
+                else if (s == "/") st.push(x / y);
+            }
         }
 
+        assert(st.size() == 1);
         return st.top();
     }
 };
