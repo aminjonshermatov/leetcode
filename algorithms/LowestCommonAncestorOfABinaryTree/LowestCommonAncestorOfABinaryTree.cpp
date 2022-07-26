8336c68a147150a7184f8438fcfd762b6b89631d
aminjonshermatov <aminjonshermatov@gmail.com>
2022-07-26 12:42:38 +0500

    LowestCommonAncestorOfABinaryTree

--- algorithms/LowestCommonAncestorOfABinaryTree/LowestCommonAncestorOfABinaryTree.cpp
+++ algorithms/LowestCommonAncestorOfABinaryTree/LowestCommonAncestorOfABinaryTree.cpp
@@ -17,14 +17,14 @@ public:
         l.push_back(node);
 
         if (node == cur) return true;
-        if (node->left != nullptr and dfs(node->left, cur, l)) return true;
-        if (node->right != nullptr and dfs(node->right, cur, l)) return true;
+        if (node->left != nullptr && dfs(node->left, cur, l)) return true;
+        if (node->right != nullptr && dfs(node->right, cur, l)) return true;
         l.pop_back();
         return false;
     }
 
     inline auto print_l(list<TreeNode*> l) -> void {
-        while (not l.empty()) {
+        while (!l.empty()) {
             cout << l.front()->val << ':';
             l.pop_front();
         }
@@ -32,7 +32,7 @@ public:
     }
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        auto curQ{root};
+        auto curQ = root;
         list<TreeNode*> lP, lQ;
 
         dfs(root, p, lP);
@@ -44,7 +44,7 @@ public:
 
         TreeNode* res = nullptr;
 
-        while (not lP.empty() and not lQ.empty() and lP.front() == lQ.front()) {
+        while (!lP.empty() && !lQ.empty() && lP.front() == lQ.front()) {
             res = lP.front();
             lP.pop_front();
             lQ.pop_front();
@@ -58,31 +58,31 @@ class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         const function<list<TreeNode*>(TreeNode*)> getPath = [&](TreeNode *node) -> list<TreeNode*> {
-            list<TreeNode*> l;
-            unordered_map<TreeNode*, int> w;
+                list<TreeNode*> l;
+                unordered_map<TreeNode*, int> w;
 
-            auto cur{root};
+                auto cur = root;
 
-            while (cur != nullptr or not l.empty()) {
-                while (cur != nullptr) {
-                    l.push_back(cur);
-                    if (cur == node) return l;
-                    cur = cur->left;
+                while (cur != nullptr || !l.empty()) {
+                    while (cur != nullptr) {
+                        l.push_back(cur);
+                        if (cur == node) return l;
+                        cur = cur->left;
+                    }
+
+                    ++w[l.back()];
+                    cur = l.back()->right;
+                    if (w[l.back()] > 1) l.pop_back();
                 }
 
-                ++w[l.back()];
-                cur = l.back()->right;
-                if (w[l.back()] > 1) l.pop_back();
-            }
-
-            return l;
+                return l;
         };
 
-        auto pL{getPath(p)};
-        auto qL{getPath(q)};
+        auto pL = getPath(p);
+        auto qL = getPath(q);
 
-        auto res{root};
-        while (not pL.empty() and not qL.empty() and pL.front() == qL.front()) {
+        auto res = root;
+        while (!pL.empty() && !qL.empty() && pL.front() == qL.front()) {
             res = pL.front();
             pL.pop_front();
             qL.pop_front();
