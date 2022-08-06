54bd25be205f0e5e5c26903dfb0733fa7231e841
aminjonshermatov <aminjonshermatov@gmail.com>
2022-08-06 23:13:36 +0500

    BinarySearch

--- algorithms/BinarySearch/BinarySearch.cpp
+++ algorithms/BinarySearch/BinarySearch.cpp
@@ -5,16 +5,19 @@ using namespace std;
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        const int n = static_cast<int>(nums.size());
-
-        for (auto lo{0}, hi{n}; lo < hi;) {
+        for (size_t lo = 0, hi = nums.size(); lo < hi) {
             auto mid = lo + ((hi - lo) >> 1);
 
-            if (nums[mid] < target) lo = mid + 1;
-            else if (nums[mid] > target) hi = mid;
+            if (nums[mid] > target) hi = mid;
+            else if (nums[mid] < target) lo = mid + 1;
             else return mid;
         }
 
         return -1;
+    }
+
+    int search(vector<int>& nums, int target) {
+        if (auto lo = lower_bound(nums.begin(), nums.end(), target); lo == nums.end() || *lo != target) return -1;
+        else return lo - nums.begin();
     }
 };
