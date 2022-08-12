b32f5d08730430687c612329c9064bfa8afaaf45
aminjonshermatov <aminjonshermatov@gmail.com>
2022-08-12 10:03:32 +0500

    LowestCommonAncestorOfABinarySearchTree

--- algorithms/LowestCommonAncestorOfABinarySearchTree/LowestCommonAncestorOfABinarySearchTree.cpp
+++ algorithms/LowestCommonAncestorOfABinarySearchTree/LowestCommonAncestorOfABinarySearchTree.cpp
@@ -29,4 +29,11 @@ public:
 
         return res;
     }
+
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (p->val > q->val) return lowestCommonAncestor(root, q, p);
+        if (root == nullptr || (root->val >= p->val && root->val <= q->val)) return root;
+        if (root->val > q->val) return lowestCommonAncestor(root->left, p, q);
+        return lowestCommonAncestor(root->right, p, q);
+    }
 };
