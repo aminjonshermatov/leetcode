0855ab7ab996cbd66913ed8332ba6fac5709230d
aminjonshermatov <aminjonshermatov@gmail.com>
2022-08-29 09:50:47 +0300

    NumberOfIslands

--- algorithms/NumberOfIslands/NumberOfIslands.cpp
+++ algorithms/NumberOfIslands/NumberOfIslands.cpp
@@ -5,25 +5,25 @@ using namespace std;
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        const int n(grid.size());
-        const int m(grid[0].size());
+        const int N(grid.size());
+        const int M(grid[0].size());
 
-        array<int, 5> dk = {1, 0, -1, 0, 1};
-        const function<void(int, int)> dfs = [&](int i, int j) {
+        array<int, 5> dk = {1,0,-1,0,1};
+        function<void(int, int)> dfs = [&](int i, int j) {
             grid[i][j] = '0';
 
             for (int k = 0; k < 4; ++k) {
                 int ii = i + dk[k];
                 int jj = j + dk[k + 1];
-
-                if (ii >= 0 && ii < n && jj >= 0 && jj < m && grid[ii][jj] != '0') dfs(ii, jj);
+                if (ii < 0 || ii >= N || jj < 0 || jj >= M || grid[ii][jj] != '1') continue;
+                dfs(ii, jj);
             }
         };
 
         int ans = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (grid[i][j] == '1') ++ans, dfs(i, j);
+        for (int i = 0; i < N; ++i) {
+            for (int j = 0; j < M; ++j) {
+                if (grid[i][j] == '1') dfs(i, j), ++ans;
             }
         }
 
