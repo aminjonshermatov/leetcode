c7fbd322544c75058fda3e23f1acd402b25c57d4
aminjonshermatov <aminjonshermatov@gmail.com>
2022-09-08 11:33:45 +0300

    BinaryTreeInorderTraversal

--- algorithms/BinaryTreeInorderTraversal/BinaryTreeInorderTraversal.cpp
+++ algorithms/BinaryTreeInorderTraversal/BinaryTreeInorderTraversal.cpp
@@ -2,25 +2,27 @@
 
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> res;
 
-        function<void(TreeNode*)> dfs = [&](TreeNode* node) -> void {
+        function<void(TreeNode*)> dfs = [&res, &dfs](TreeNode* node) {
             if (node == nullptr) return;
 
             dfs(node->left);
-            res.push_back(node->val);
+            res.emplace_back(node->val);
             dfs(node->right);
         };
 
