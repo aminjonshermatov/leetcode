db175ac4f984af46608701d9c23eba1c7922d14f
aminjonshermatov <aminjonshermatov@gmail.com>
2022-09-21 12:34:09 +0300

    SumOfEvenNumbersAfterQueries

--- algorithms/SumOfEvenNumbersAfterQueries/SumOfEvenNumbersAfterQueries.cpp
+++ algorithms/SumOfEvenNumbersAfterQueries/SumOfEvenNumbersAfterQueries.cpp
@@ -13,14 +13,9 @@ public:
         for (const auto &q : queries) {
             auto newV = abs(nums[q[1]] + q[0]);
 
-            if (newV & 1) {
-                if ((nums[q[1]] & 1) == 0) even -= nums[q[1]];
-                nums[q[1]] += q[0];
-            } else {
-                if ((nums[q[1]] & 1) == 0) even -= nums[q[1]];
-                nums[q[1]] += q[0];
-                even += nums[q[1]];
-            }
+            if ((nums[q[1]] & 1) == 0) even -= nums[q[1]];
+            nums[q[1]] += q[0];
+            if ((newV & 1) == 0) even += nums[q[1]];
 
             res.emplace_back(even);
         }
