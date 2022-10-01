b3a997b3d6a44707782dc66dfa67ee3516be4721
aminjonshermatov <aminjonshermatov@gmail.com>
2022-10-01 17:43:39 +0300

    DecodeWays

--- algorithms/DecodeWays/DecodeWays.cpp
+++ algorithms/DecodeWays/DecodeWays.cpp
@@ -5,17 +5,13 @@ using namespace std;
 class Solution {
 public:
     int numDecodings(string s) {
-        if (s.empty() || *s.begin() == '0') return 0;
+        const int N(s.size());
 
-        vector<int> dp(s.size() + 2, 0);
-        dp[s.size()] = 1;
-
-        for (int i = s.size() - 1; i >= 0; --i) {
-            if (s[i] <= '0' || s[i] > '9') continue;
-
-            dp[i] += dp[i + 1];
-
-            if (i + 1 < s.size() && (s[i] == '1' || s[i] == '2' && s[i + 1] < '7')) dp[i] += dp[i + 2];
+        vector<int> dp(N + 2, 0);
+        dp[N] = 1;
+        for (int i = N - 1; i >= 0; --i) {
+            if (s[i] != '0') dp[i] += dp[i + 1];
+            if (s[i] > '0' && s[i] < '2' || (s[i] == '2' && s[i + 1] <= '6')) dp[i] += dp[i + 2];
         }
 
         return dp[0];
