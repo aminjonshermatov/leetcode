4422aadaecf2c968bca1fd2142d4119bdfd16252
aminjonshermatov <aminjonshermatov@gmail.com>
2022-10-11 11:40:50 +0300

    IncreasingTripletSubsequence

--- algorithms/IncreasingTripletSubsequence/IncreasingTripletSubsequence.cpp
+++ algorithms/IncreasingTripletSubsequence/IncreasingTripletSubsequence.cpp
@@ -5,20 +5,12 @@ using namespace std;
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        const int n = static_cast<int>(nums.size());
-
-        if (n < 3) return false;
-
-        int a = INT_MAX, b = INT_MAX;
-
-        for (const auto num : nums) {
-            if (num < a) {
-                a = num;
-            } else if (num < b and num > a) {
-                b = num;
-            } else if (num > b) {
-                return true;
-            }
+        int a, b;
+        a = b = numeric_limits<int>::max();
+        for (auto num : nums) {
+            if (num < a) a = num;
+            else if (num < b && num > a) b = num;
+            else if (num > b) return true;
         }
 
         return false;
