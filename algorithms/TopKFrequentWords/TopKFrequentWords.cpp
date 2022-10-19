ad423e37eed780480557192ee643ba102d596315
aminjonshermatov <aminjonshermatov@gmail.com>
2022-10-19 18:31:56 +0300

    TopKFrequentWords

--- algorithms/TopKFrequentWords/TopKFrequentWords.cpp
+++ algorithms/TopKFrequentWords/TopKFrequentWords.cpp
@@ -27,4 +27,30 @@ public:
 
         return res;
     }
+
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        map<string, int> freq;
+        for (auto &w : words) ++freq[w];
+
+        auto cmp = [&](auto &l, auto &r) {
+            auto lf = freq[l];
+            auto rf = freq[r];
+            return lf == rf ? l < r : lf > rf;
+        };
+        priority_queue<string, vector<string>, decltype(cmp)> pq(cmp);
+
+        for (auto &[w, _] : freq) {
+            pq.push(w);
+            if (pq.size() > k) pq.pop();
+        }
+
+        vector<string> res;
+        while (!pq.empty()) {
+            res.push_back(pq.top());
+            pq.pop();
+        }
+        reverse(res.begin(), res.end());
+
+        return res;
+    }
 };
