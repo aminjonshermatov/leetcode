9f7422e444598a22936894452ece25aae73b5697
aminjonshermatov <aminjonshermatov@gmail.com>
2022-11-17 12:02:00 +0300

    RectangleArea

--- algorithms/RectangleArea/RectangleArea.cpp
+++ algorithms/RectangleArea/RectangleArea.cpp
@@ -5,17 +5,16 @@ using namespace std;
 class Solution {
 public:
     int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        int res = (ax2 - ax1) * (ay2 - ay1) + (bx2 - bx1) * (by2 - by1);
+        int total = (ax2 - ax1) * (ay2 - ay1) + (bx2 - bx1) * (by2 - by1);
 
-        int overl[4];
+        array<int, 4> overlap = {
+                max(ax1, bx1),
+                max(ay1, by1),
+                min(ax2, bx2),
+                min(ay2, by2)
+        };
 
-        overl[0] = max(ax1, bx1);
-        overl[1] = max(ay1, by1);
-        overl[2] = min(ax2, bx2);
-        overl[3] = min(ay2, by2);
-
-        if (overl[0] < overl[2] and overl[1] < overl[3]) res -= (overl[2] - overl[0]) * (overl[3] - overl[1]);
-
-        return res;
+        if (overlap[0] <= overlap[2] && overlap[1] <= overlap[3]) total -= (overlap[2] - overlap[0]) * (overlap[3] - overlap[1]);
+        return total;
     }
 };
