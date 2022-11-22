9acbfab21cbdc32bb21b88172d4e8c2c05ef4db2
aminjonshermatov <aminjonshermatov@gmail.com>
2022-11-22 20:56:24 +0300

    PerfectSquares

--- algorithms/PerfectSquares/PerfectSquares.cpp
+++ algorithms/PerfectSquares/PerfectSquares.cpp
@@ -3,16 +3,13 @@
 using namespace std;
 
 class Solution {
+    static inline constexpr int inf = numeric_limits<int>::max();
 public:
     int numSquares(int n) {
-        vector<int> dp(n + 1, INT_MAX);
+        vector<int> dp(n + 1, inf);
         dp[0] = 0;
-
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j * j <= n; ++j) {
-                if (i - j * j >= 0) dp[i] = min(dp[i], dp[i - j * j] + 1);
-            }
-        }
+        for (int i = 0; i <= n; ++i)
+            for (int j = 1; j * j <= i; ++j) dp[i] = min(dp[i], dp[i - j * j] + 1);
 
         return dp[n];
     }
