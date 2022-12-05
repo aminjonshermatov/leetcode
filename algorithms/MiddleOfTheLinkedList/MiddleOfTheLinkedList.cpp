a027461e74ae3e3089bae5b32d45f5f5ddc255cc
aminjonshermatov <aminjonshermatov@gmail.com>
2022-12-05 10:39:53 +0300

    MiddleOfTheLinkedList

--- algorithms/MiddleOfTheLinkedList/MiddleOfTheLinkedList.cpp
+++ algorithms/MiddleOfTheLinkedList/MiddleOfTheLinkedList.cpp
@@ -2,21 +2,23 @@
 
 using namespace std;
 
-struct ListNode {
-     int val;
-     ListNode *next;
-     ListNode() : val(0), next(nullptr) {}
-     explicit ListNode(int x) : val(x), next(nullptr) {}
-     ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        auto slow{head};
-        auto fast{head};
+        auto slow = head;
+        auto fast = head;
 
-        while (fast != nullptr and fast->next != nullptr) {
+        while (fast != nullptr && fast->next != nullptr) {
             slow = slow->next;
             fast = fast->next->next;
         }
