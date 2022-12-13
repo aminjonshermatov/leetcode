8d1ea897521cd3f469ed779621522a8b2a5202d0
aminjonshermatov <aminjonshermatov@gmail.com>
2022-12-13 13:52:15 +0300

    MinimumFallingPathSum

--- algorithms/MinimumFallingPathSum/MinimumFallingPathSum.cpp
+++ algorithms/MinimumFallingPathSum/MinimumFallingPathSum.cpp
@@ -4,24 +4,19 @@ using namespace std;
 
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& matrix) {
-        int n = matrix.size();
-
-        vector<int> prev(matrix[0]), cur(n);
+    int minFallingPathSum(vector<vector<int>>& A) {
+        const int n(A.size());
+        const int m(A[0].size());
 
         for (int i = 1; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                cur[j] = prev[j];
-
-                if (j > 0) cur[j] = min(cur[j], prev[j - 1]);
-                if (j + 1 < n) cur[j] = min(cur[j], prev[j + 1]);
-
-                cur[j] += matrix[i][j];
+            for (int j = 0; j < m; ++j) {
+                int mn = A[i - 1][j];
+                if (j > 0) mn = min(mn, A[i - 1][j - 1]);
+                if (j + 1 < m) mn = min(mn, A[i - 1][j + 1]);
+                A[i][j] += mn;
             }
-
-            prev = cur;
         }
 
-        return *min_element(prev.begin(), prev.end());
+        return *min_element(A[n - 1].begin(), A[n - 1].end());
     }
 };
