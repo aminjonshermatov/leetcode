def40790f671e754e63b63d1a859c5387d13b9ad
aminjonshermatov <aminjonshermatov@gmail.com>
2022-12-27 11:12:58 +0300

    MaximumBagsWithFullCapacityOfRocks

--- algorithms/MaximumBagsWithFullCapacityOfRocks/MaximumBagsWithFullCapacityOfRocks.cpp
+++ algorithms/MaximumBagsWithFullCapacityOfRocks/MaximumBagsWithFullCapacityOfRocks.cpp
@@ -10,13 +10,11 @@ public:
         vector<int> A(n);
         iota(A.begin(), A.end(), 0);
         sort(A.begin(), A.end(), [&](auto lhs, auto rhs) { return c[lhs] - r[lhs] < c[rhs] - r[rhs]; });
-
-        for (auto i : A) {
+        for_each(A.begin(), A.end(), [&](auto i) {
             auto to_add = min(add, c[i] - r[i]);
             r[i] += to_add;
-            if ((add -= to_add) == 0) break;
-        }
-
+            add -= to_add;
+        });
         return count_if(A.begin(), A.end(), [&](auto i) { return r[i] == c[i]; });
     }
 };
