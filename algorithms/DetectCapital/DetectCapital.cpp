425aa3cff82f8676b3b60e80b19e3bcc7b3cfc8e
aminjonshermatov <aminjonshermatov@gmail.com>
2023-01-02 11:53:24 +0300

    DetectCapital

--- algorithms/DetectCapital/DetectCapital.cpp
+++ algorithms/DetectCapital/DetectCapital.cpp
@@ -5,12 +5,8 @@ using namespace std;
 class Solution {
 public:
     bool detectCapitalUse(string word) {
-        int upperCount = 0;
-
-        for (const auto ch : word) {
-            if (ch >= 'A' and ch <= 'Z') ++upperCount;
-        }
-
-        return upperCount == word.size() or upperCount == 0 or (upperCount == 1 and word[0] >= 'A' and word[0] <= 'Z');
+        return all_of(word.begin(), word.end(), [](auto ch) { return isupper(ch); })
+               || all_of(word.begin(), word.end(), [](auto ch) { return islower(ch); })
+               || (all_of(next(word.begin()), word.end(), [](auto ch) { return islower(ch); }) && isupper(word.front()));
     }
 };
