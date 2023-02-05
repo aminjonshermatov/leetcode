91f05cb3c56ed5d10a85c0512299528aa384d33d
aminjonshermatov <aminjonshermatov@gmail.com>
2023-02-05 11:19:14 +0300

    FindAllAnagramsInAString

--- algorithms/FindAllAnagramsInAString/FindAllAnagramsInAString.cpp
+++ algorithms/FindAllAnagramsInAString/FindAllAnagramsInAString.cpp
@@ -1,32 +1,25 @@
-#include "bits/stdc++.h"
-
-using namespace std;
-
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
         const int n(s.size());
         const int k(p.size());
 
-        int f1[26] = {0,}, f2[26] = {0,};
+        array<int, 26> f1{}, f2{};
+        fill(f1.begin(), f1.end(), 0);
+        fill(f2.begin(), f2.end(), 0);
 
         for (const auto ch : p) ++f2[ch - 'a'];
 
-        const function<bool()> isEq = [&]() {
-            for (int i = 0; i < 26; ++i) {
-                if (f1[i] != f2[i]) return false;
-            }
-
+        auto ok = [&]() {
+            for (int i = 0; i < 26; ++i) if (f1[i] != f2[i]) return false;
             return true;
         };
 
         vector<int> res;
         for (int ss = 0, ee = 0; ee < n; ++ee) {
             ++f1[s[ee] - 'a'];
-
             if (ee - ss >= k) --f1[s[ss++] - 'a'];
-
-            if (isEq()) res.push_back(ss);
+            if (ok()) res.emplace_back(ss);
         }
 
         return res;
