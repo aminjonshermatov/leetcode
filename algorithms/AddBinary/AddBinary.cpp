4a4ea0efa7bcd97d0e4164a10c912b681f051c7a
aminjonshermatov <aminjonshermatov@gmail.com>
2023-02-14 11:45:32 +0300

    AddBinary

--- algorithms/AddBinary/AddBinary.cpp
+++ algorithms/AddBinary/AddBinary.cpp
@@ -4,36 +4,25 @@ using namespace std;
 
 class Solution {
 public:
-    string addBinary(string a, string b) {
-        int aR = a.size() - 1,
-            bR = b.size() - 1,
-            rem = 0;
-
-        string res;
-
-        while (aR >= 0 && bR >= 0) {
-            int temp = a[aR--] - '0' + b[bR--] - '0' + rem;
-
-            res.insert(res.begin(), (temp % 2 != 0 ? '1' : '0'));
-            rem = temp / 2;
-        }
-
-        while (aR >= 0) {
-            int temp = a[aR--] - '0' + rem;
-
-            res.insert(res.begin(), (temp % 2 != 0 ? '1' : '0'));
-            rem = temp / 2;
-        }
-
-        while (bR >= 0) {
-            int temp = b[bR--] - '0' + rem;
-
-            res.insert(res.begin(), (temp % 2 != 0 ? '1' : '0'));
-            rem = temp / 2;
-        }
-
-        if (rem != 0) res.insert(res.begin(), '1');
-
-        return res;
+  string addBinary(string a, string b) {
+    reverse(a.begin(), a.end());
+    reverse(b.begin(), b.end());
+    const int n(a.size());
+    const int m(b.size());
+    string c;
+    int rem = 0;
+    for (int i = 0, j = 0; i < n || j < m; ++i, ++j) {
+      int cur = rem;
+      if (i < n) cur += a[i] - '0';
+      if (j < m) cur += b[j] - '0';
+      c.push_back('0' + cur % 2);
+      rem = cur / 2;
     }
+    while (rem) {
+      c.push_back('0' + rem % 2);
+      rem /= 2;
+    }
+    reverse(c.begin(), c.end());
+    return c;
+  }
 };
