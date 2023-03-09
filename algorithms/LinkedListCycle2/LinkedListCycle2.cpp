840f43b22bff8a30f201fcd7cbf12458f8db71a4
aminjonshermatov <aminjonshermatov@gmail.com>
2023-03-09 18:21:10 +0300

    LinkedListCycle2

--- algorithms/LinkedListCycle2/LinkedListCycle2.cpp
+++ algorithms/LinkedListCycle2/LinkedListCycle2.cpp
@@ -2,34 +2,32 @@
 
 using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode *detectCycle(ListNode *head) {
-        if (head == nullptr || head->next == nullptr) return nullptr;
-
-        ListNode *slow = head, *fast = head;
-
-        while (fast != nullptr && fast->next != nullptr) {
-            slow = slow->next;
-            fast = fast->next->next;
-
-            if (slow == fast) break;
-        }
-
-        if (slow != fast) return nullptr;
-
-        slow = head;
-        while (slow != fast) {
-            slow = slow->next;
-            fast = fast->next;
-        }
-
-        return slow;
+  ListNode *detectCycle(ListNode *head) {
+    if (head == nullptr || head->next == nullptr) return nullptr;
+    auto slow = head;
+    auto fast = head;
+    while (fast != nullptr && fast->next != nullptr) {
+      slow = slow->next;
+      fast = fast->next->next;
+      if (slow == fast) break;
     }
+
+    if (slow != fast) return nullptr;
+    slow = head;
+    while (slow != fast) {
+      slow = slow->next;
+      fast = fast->next;
+    }
+    return slow;
+  }
 };
