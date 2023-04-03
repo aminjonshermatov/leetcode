6b534d122e77e864b77675e778f6fa782b8c08f3
aminjonshermatov <aminjonshermatov@gmail.com>
2023-04-03 19:53:30 +0300

    BoatsToSavePeople

--- algorithms/BoatsToSavePeople/BoatsToSavePeople.cpp
+++ algorithms/BoatsToSavePeople/BoatsToSavePeople.cpp
@@ -4,26 +4,13 @@ using namespace std;
 
 class Solution {
 public:
-    int numRescueBoats(vector<int>& people, int limit) {
-        const int n = static_cast<int>(people.size());
-
-        sort(people.begin(), people.end());
-
-        auto l{0};
-        auto r{n - 1};
-        auto count{0};
-
-        while (l <= r) {
-            if (people[l] + people[r] <= limit) {
-                ++count;
-                ++l;
-                --r;
-            } else {
-                --r;
-                ++count;
-            }
-        }
-
-        return count;
+  int numRescueBoats(vector<int>& P, int limit) {
+    const int n(P.size());
+    sort(P.begin(), P.end());
+    int ans = 0;
+    for (int l = 0, r = n - 1; l <= r; ++ans, --r) {
+      if (P[l] + P[r] <= limit) ++l;
     }
+    return ans;
+  }
 };
