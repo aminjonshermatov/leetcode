85376d335085fa687859c35a7e4d409adce7459a
aminjonshermatov <aminjonshermatov@gmail.com>
2023-04-07 12:41:16 +0300

    NumberOfEnclaves

--- algorithms/NumberOfEnclaves/NumberOfEnclaves.cpp
+++ algorithms/NumberOfEnclaves/NumberOfEnclaves.cpp
@@ -3,35 +3,43 @@
 using namespace std;
 
 class Solution {
+  static inline constexpr array<int, 5> dk{1, 0, -1, 0, 1};
 public:
-    int numEnclaves(vector<vector<int>>& grid) {
-        const int r = static_cast<int>(grid.size());
-        const int c = static_cast<int>(grid[0].size());
+  int numEnclaves(vector<vector<int>>& grid) {
+    const int n(grid.size());
+    const int m(grid[0].size());
 
-        const function<int(const int, const int)> dfs = [&](const int y, const int x) -> int {
-            if (y < 0 or y >= r or x < 0 or x >= c or grid[y][x] == 0) return 0;
+    auto is_good = [&](int i, int j) -> bool {
+      return clamp(i, 0, n - 1) == i && clamp(j, 0, m - 1) == j;
+    };
 
-            grid[y][x] = 0;
-            return 1 + dfs(y - 1, x) + dfs(y + 1, x) + dfs(y, x - 1) + dfs(y, x + 1);
-        };
+    auto dfs = [&](auto f, int i, int j) -> void {
+      grid[i][j] = 2;
+      for (int k = 0; k < 4; ++k) {
+        auto ni = i + dk[k];
+        auto nj = j + dk[k + 1];
+        if (!is_good(ni, nj) || grid[ni][nj] != 1) continue;
+        f(f, ni, nj);
+      }
+    };
 
-        for (int i = 0; i < r; ++i) {
-            if (grid[i][0] == 1) dfs(i, 0);
-            if (grid[i][c - 1] == 1) dfs(i, c - 1);
-        }
-
-        for (int j = 0; j < c; ++j) {
-            if (grid[0][j] == 1) dfs(0, j);
-            if (grid[r - 1][j] == 1) dfs(r - 1, j);
-        }
-
-        int res = 0;
-        for (int i = 1; i < r - 1; ++i) {
-            for (int j = 1; j < c - 1; ++j) {
-                if (grid[i][j] == 1) res += dfs(i, j);
-            }
-        }
-
-        return res;
+    for (int i = 0; i < n; ++i) {
+      for (auto j : {0, m - 1}) {
+        if (grid[i][j] == 1) dfs(dfs, i, j);
+      }
     }
+    for (int j = 0; j < m; ++j) {
+      for (auto i : {0, n - 1}) {
+        if (grid[i][j] == 1) dfs(dfs, i, j);
+      }
+    }
+
+    int ans = 0;
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < m; ++j) {
+        ans += grid[i][j] == 1;
+      }
+    }
+    return ans;
+  }
 };
