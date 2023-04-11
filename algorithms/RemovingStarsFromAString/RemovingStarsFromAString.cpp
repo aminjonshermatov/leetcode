1c52930640ebf72efd84279516e8acab956ee9bf
aminjonshermatov <aminjonshermatov@gmail.com>
2023-04-11 12:30:36 +0300

    RemovingStarsFromAString

--- algorithms/RemovingStarsFromAString/RemovingStarsFromAString.cpp
+++ algorithms/RemovingStarsFromAString/RemovingStarsFromAString.cpp
@@ -4,18 +4,22 @@ using namespace std;
 
 class Solution {
 public:
-    string removeStars(string s) {
-        stack<char> st;
-
-        for (auto ch : s) {
-            if (ch == '*') st.pop();
-            else st.push(ch);
-        }
-
-        string ans;
-        ans.resize(st.size());
-        for (int i = (int)st.size() - 1; i >= 0; --i) ans[i] = st.top(), st.pop();
-
-        return ans;
+  string removeStars(string s) {
+    stack<char> st;
+    for (auto ch : s) {
+      if (ch == '*') {
+        assert(!st.empty());
+        st.pop();
+      } else {
+        st.push(ch);
+      }
     }
+    string res;
+    while (!st.empty()) {
+      res.push_back(st.top());
+      st.pop();
+    }
+    reverse(res.begin(), res.end());
+    return res;
+  }
 };
