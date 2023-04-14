b2bc227c7e13d98adcc659d16e01947cc8458966
aminjonshermatov <aminjonshermatov@gmail.com>
2023-04-14 10:54:38 +0300

    LongestPalindromicSubsequence

--- algorithms/LongestPalindromicSubsequence/LongestPalindromicSubsequence.cpp
+++ algorithms/LongestPalindromicSubsequence/LongestPalindromicSubsequence.cpp
@@ -4,20 +4,23 @@ using namespace std;
 
 class Solution {
 public:
-    int longestPalindromeSubseq(string s) {
-        const int n = s.size();
-
-        vector<vector<int>> dp(n, vector<int>(n));
-
-        for (int i = 0; i < n; ++i) dp[i][i] = 1;
-
-        for (int i = 1; i < n; ++i) {
-            for (int j = 0; i + j < n; ++j) {
-                if (s[j] == s[i + j]) dp[j][i + j] = 2 + dp[j + 1][i + j - 1];
-                else dp[j][i + j] = max(dp[j + 1][i + j], dp[j][i + j - 1]);
-            }
-        }
-
-        return dp[0][n - 1];
+  int longestPalindromeSubseq(string s) {
+    const int n(s.size());
+    vector<vector<int>> dp(n, vector<int>(n, 0));
+    for (int i = 0; i < n; ++i) {
+      dp[i][i] = 1;
     }
+
+    for (int len = 1; len < n; ++len) {
+      for (int l = 0; l + len < n; ++l) {
+        if (s[l] == s[l + len]) {
+          dp[l][l + len] = max(dp[l][l + len], 2 + dp[l + 1][l + len - 1]);
+        } else {
+          auto nw = max(dp[l + 1][l + len], dp[l][l + len - 1]);
+          dp[l][l + len] = max(dp[l][l + len], nw);
+        }
+      }
+    }
+    return dp[0][n - 1];
+  }
 };
