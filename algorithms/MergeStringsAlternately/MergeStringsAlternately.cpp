2905105d1f4c494b650051d19bb0a53664dd12d7
aminjonshermatov <aminjonshermatov@gmail.com>
2023-04-18 12:12:55 +0300

    MergeStringsAlternately

--- algorithms/MergeStringsAlternately/MergeStringsAlternately.cpp
+++ algorithms/MergeStringsAlternately/MergeStringsAlternately.cpp
@@ -4,26 +4,22 @@ using namespace std;
 
 class Solution {
 public:
-    string mergeAlternately(string word1, string word2) {
-        list<char> merged;
-
-        bool isFirst = true;
-        const int n = static_cast<int>(word1.size());
-        const int m = static_cast<int>(word2.size());
-
-        auto i{0};
-        auto j{0};
-
-        while (i < n and j < m) {
-            if (isFirst) merged.push_back(word1[i++]);
-            else merged.push_back(word2[j++]);
-
-            isFirst = not isFirst;
-        }
-
-        while (i < n) merged.push_back(word1[i++]);
-        while (j < m) merged.push_back(word2[j++]);
-
-        return {merged.begin(), merged.end()};
+  string mergeAlternately(string word1, string word2) {
+    const int n(word1.size());
+    const int m(word2.size());
+    string res(n + m, '#');
+    for (int k = 0, i = 0, j = 0; k < n + m; ++k) {
+      if (i < n && j < m) {
+        res[k] = k & 1 ? word2[j++] : word1[i++];
+      } else if (i < n) {
+        res[k] = word1[i++];
+      } else if (j < m) {
+        res[k] = word2[j++];
+      } else {
+        assert(false);
+      }
     }
+
+    return res;
+  }
 };
