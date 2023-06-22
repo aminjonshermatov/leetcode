3386641be68a607526c5a04dde84adc863bb6191
aminjonshermatov <aminjonshermatov@gmail.com>
2023-06-22 10:24:39 +0300

    BestTimeToBuyAndSellStockWithTransactionFee

--- algorithms/BestTimeToBuyAndSellStockWithTransactionFee/BestTimeToBuyAndSellStockWithTransactionFee.cpp
+++ algorithms/BestTimeToBuyAndSellStockWithTransactionFee/BestTimeToBuyAndSellStockWithTransactionFee.cpp
@@ -4,35 +4,35 @@ using namespace std;
 
 class Solution {
 public:
-    int maxProfit(vector<int>& prices, int fee) {
-        vector<array<int, 2>> dp(prices.size(), {-1, -1});
+  int maxProfit(vector<int> &ps, int fee) {
+    const int n(ps.size());
+    vector<array<int, 2>> cache(n, array<int, 2>{0, 0});
+    vector<array<bool, 2>> seen(n, array<bool, 2>{false, false});
+    auto dp = [&](auto &f, int day, bool buy) -> int {
+      if (day >= n)
+        return buy ? 0 : -1e9;
+      if (seen[day][buy])
+        return cache[day][buy];
 
-        function<int(int, bool)> dfs = [&](int idx, bool isBuying) -> int {
-            if (idx >= prices.size()) return 0;
+      auto loc = f(f, day + 1, buy);
+      if (buy)
+        loc = max(loc, f(f, day + 1, false) - ps[day]);
+      else
+        loc = max(loc, f(f, day + 1, true) + ps[day] - fee);
+      seen[day][buy] = true;
+      return cache[day][buy] = loc;
+    };
+    return dp(dp, 0, true);
+  }
 
-            if (dp[idx][static_cast<int>(isBuying)] != -1) return dp[idx][static_cast<int>(isBuying)];
+  int maxProfit(vector<int> &prices, int fee) {
+    int cash = 0, hold = -prices[0];
 
-            int pass = dfs(idx + 1, isBuying);
-            if (isBuying) {
-                dp[idx][static_cast<int>(isBuying)] = max(pass, dfs(idx + 1, not isBuying) - prices[idx]);
-            } else {
-                dp[idx][static_cast<int>(isBuying)] = max(pass, dfs(idx + 1, not isBuying) + prices[idx] - fee);
-            }
-
-            return dp[idx][static_cast<int>(isBuying)];
-        };
-
-        return dfs(0, true);
+    for (size_t i{1}; i < prices.size(); ++i) {
+      cash = max(cash, hold + prices[i] - fee);
+      hold = max(hold, cash - prices[i]);
     }
 
-    int maxProfit(vector<int>& prices, int fee) {
-        int cash = 0, hold = -prices[0];
-
-        for (size_t i{1}; i < prices.size(); ++i) {
-            cash = max(cash, hold + prices[i] - fee);
-            hold = max(hold, cash - prices[i]);
-        }
-
-        return cash;
-    }
+    return cash;
+  }
 };
