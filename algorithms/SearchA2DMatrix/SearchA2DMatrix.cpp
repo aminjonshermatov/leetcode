064b50b493f18d90459dda285cef417ed0365a5a
aminjonshermatov <aminjonshermatov@gmail.com>
2023-08-07 11:57:44 +0300

    SearchA2DMatrix

--- algorithms/SearchA2DMatrix/SearchA2DMatrix.cpp
+++ algorithms/SearchA2DMatrix/SearchA2DMatrix.cpp
@@ -4,70 +4,73 @@ using namespace std;
 
 class Solution {
 public:
-    // O(log(r + c)) - TC, O(1) - SC
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int r = matrix.size(),
-            c = matrix[0].size();
+  // O(log(r + c)) - TC, O(1) - SC
+  bool searchMatrix(vector<vector<int>> &g, int t) {
+    const int n(g.size());
+    const int m(g[0].size());
+    int lo = -1, hi = n * m;
+    while (hi - lo > 1) {
+      auto mid = lo + (hi - lo) / 2;
+      (g[mid / m][mid % m] > t ? hi : lo) = mid;
+    }
+    return lo >= 0 && g[lo / m][lo % m] == t;
+  }
 
-        if (matrix[0][0] > target or matrix[r - 1][c - 1] < target) return false;
+  // O(log(r) + log(c)) - TC, O(1) - SC
+  bool searchMatrix(vector<vector<int>> &mat, int target) {
+    const int n = static_cast<int>(mat.size());
+    const int m = static_cast<int>(mat[0].size());
 
-        int lo = 0, hi =r * c;
+    auto lo{0};
+    auto hi{n};
 
-        while (lo < hi) {
-            int mid = lo + (hi - lo) / 2;
+    while (lo < hi) {
+      const auto mid{lo + ((hi - lo) >> 1)};
 
-            if (matrix[mid / c][mid % c] < target) lo = mid + 1;
-            else if (matrix[mid / c][mid % c] > target) hi = mid;
-            else return true;
-        }
-
-        return false;
+      if (mat[mid][0] > target)
+        hi = mid;
+      else
+        lo = mid + 1;
     }
 
-    // O(log(r) + log(c)) - TC, O(1) - SC
-    bool searchMatrix(vector<vector<int>>& mat, int target) {
-        const int n = static_cast<int>(mat.size());
-        const int m = static_cast<int>(mat[0].size());
+    const auto it{hi - 1};
 
-        auto lo{0};
-        auto hi{n};
+    if (it >= 0 and it < n) {
+      lo = 0;
+      hi = m;
 
-        while (lo < hi) {
-            const auto mid{lo + ((hi - lo) >> 1)};
+      while (lo < hi) {
+        const auto mid{lo + ((hi - lo) >> 1)};
 
-            if (mat[mid][0] > target) hi = mid;
-            else lo = mid + 1;
-        }
-
-        const auto it{hi - 1};
-
-        if (it >= 0 and it < n) {
-            lo = 0;
-            hi = m;
-
-            while (lo < hi) {
-                const auto mid{lo + ((hi - lo) >> 1)};
-
-                if (mat[it][mid] > target) hi = mid;
-                else if (mat[it][mid] < target) lo = mid + 1;
-                else return true;
-            }
-        }
-
-        return false;
+        if (mat[it][mid] > target)
+          hi = mid;
+        else if (mat[it][mid] < target)
+          lo = mid + 1;
+        else
+          return true;
+      }
     }
+
+    return false;
+  }
 };
 
 class Solution {
-    inline bool low_b(const vector<int> &row, int t) const {
-        if (auto lo = lower_bound(row.begin(), row.end(), t); lo == row.end() || *lo != t) return false;
-        return true;
-    }
-public:
-    bool searchMatrix(vector<vector<int>>& mat, int t) {
-        auto up = upper_bound(mat.begin(), mat.end(), t, [](auto tt, const auto &row) { return tt < row.front(); });
+  inline bool low_b(const vector<int> &row, int t) const {
+    if (auto lo = lower_bound(row.begin(), row.end(), t);
+        lo == row.end() || *lo != t)
+      return false;
+    return true;
+  }
 
-        if (up == mat.begin() || up == mat.end()) return low_b(mat.back(), t);
-        return low_b(*prev(up), t);
-    }
+public:
+  bool searchMatrix(vector<vector<int>> &mat, int t) {
+    auto up =
+        upper_bound(mat.begin(), mat.end(), t,
+                    [](auto tt, const auto &row) { return tt < row.front(); });
+
+    if (up == mat.begin() || up == mat.end())
+      return low_b(mat.back(), t);
+    return low_b(*prev(up), t);
+  }
 };
