9ff279bbffc361ee8387651533d3b6b41e1e718f
aminjonshermatov <aminjonshermatov@gmail.com>
2023-08-14 10:31:15 +0300

    KthLargestElementInAnArray

--- algorithms/KthLargestElementInAnArray/KthLargestElementInAnArray.cpp
+++ algorithms/KthLargestElementInAnArray/KthLargestElementInAnArray.cpp
@@ -4,12 +4,25 @@ using namespace std;
 
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
+  int findKthLargest(vector<int> &nums, int k) {
+    sort(nums.begin(), nums.end());
 
-        for (int i = static_cast<int>(nums.size()); i >= 0 and k >= 0; --i, --k) {
-            if (k == 0) return nums[i];
-        }
-        return {};
+    for (int i = static_cast<int>(nums.size()); i >= 0 and k >= 0; --i, --k) {
+      if (k == 0)
+        return nums[i];
     }
+    return {};
+  }
+  int findKthLargest(vector<int> &as, int k) {
+    int lo = -1e5, hi = 1e5;
+    while (hi - lo > 1) {
+      auto mid = lo + (hi - lo) / 2;
+      int cnt = 0;
+      for (auto a : as) {
+        cnt += a >= mid;
+      }
+      (cnt >= k ? lo : hi) = mid;
+    }
+    return lo;
+  }
 };
