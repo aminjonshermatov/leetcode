a1f45946a536f976bbdee767115b3a12b3028788
aminjonshermatov <aminjonshermatov@gmail.com>
2023-08-18 21:16:40 +0300

    MaximalNetworkRank

--- algorithms/MaximalNetworkRank/MaximalNetworkRank.cpp
+++ algorithms/MaximalNetworkRank/MaximalNetworkRank.cpp
@@ -4,25 +4,18 @@ using namespace std;
 
 class Solution {
 public:
-    int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        vector<vector<int>> adj(n, vector<int>(n, 0));
-        vector<int> rank(n, 0);
-
-        for (const auto road : roads) {
-            adj[road[0]][road[1]] = adj[road[1]][road[0]] = 1;
-            ++rank[road[0]];
-            ++rank[road[1]];
-        }
-
-        int res = 0;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (i == j) continue;
-
-                res = max(res, rank[i] + rank[j] - adj[i][j]);
-            }
-        }
-
-        return res;
+  int maximalNetworkRank(int n, vector<vector<int>>& rs) {
+    vector<set<int>> g(n);
+    for (auto &r : rs) {
+      g[r[0]].emplace(r[1]);
+      g[r[1]].emplace(r[0]);
     }
+    int ans = 0;
+    for (int v = 0; v < n; ++v) {
+      for (int u = v + 1; u < n; ++u) {
+        ans = max<int>(ans, g[u].size() + g[v].size() - g[v].count(u));
+      }
+    }
+    return ans;
+  }
 };
